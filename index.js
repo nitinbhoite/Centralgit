729c4837f549db14c83b1692c54f621f7caf997b
nitinbhoite <106171631+nitinbhoite@users.noreply.github.com>
2022-07-20 11:07:47 +0530

    Updated index.js file

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const port = process.env.PORT || 3000 ;
 const config = require('config')
 console.log(config);
 
+app.get('/', (req, res) => {
+  res.send('CICD App V2!')
+})
+
+app.get('/status', (req, res) => {
+    res.send('ok')
+  })
 
 
 app.listen(port, () => {
